d49099299c97ea144624c912dfd99551c19f0024
2412322029 <2412322029@qq.com>
2025-01-04 20:48:42 +0800

    init

---
+++ main.py
@@ -0,0 +1,121 @@
+import json
+import os
+import re
+import toml
+from flask import Flask, render_template, request
+from urllib.parse import unquote
+
+from mcinfo import mcinfo, mclatency
+
+app = Flask(__name__)
+
+data_file = 'data.json'
+Data_limit = 6
+
+
+def load_secret_key():
+    try:
+        return toml.load("config.toml").get("SECRET_KEY")
+    except ImportError:
+        print("config.toml 文件不存在或 SECRET_KEY 未定义。")
+        with open('config.toml', 'w') as f:
+            f.write("# SECRET_KEY = 'your SECRET_KEY'\n")
+        print("config.toml 文件已创建，在config.toml中设置SECRET_KEY")
+        exit()
+
+
+SECRET_KEY = load_secret_key()
+
+
+def load_data():
+    if os.path.exists(data_file):
+        with open(data_file, 'r') as f:
+            return json.load(f)
+    return {"pc": [], "browser": [], "phone": []}
+
+
+def save_data(d):
+    with open(data_file, 'w') as f:
+        json.dump(d, f, indent=4)
+
+
+data = load_data()
+
+
+def limit(d: list):
+    if len(d) > Data_limit:
+        del d[:-Data_limit]
+
+
+def is_valid_address(address: str) -> bool:
+    pattern = r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+):([0-9]{1,5})$'
+    return re.match(pattern, address) is not None
+
+
+@app.route('/get_mcinfo/<address>')
+def get_mcinfo(address: str):
+    if is_valid_address(address):
+        return mcinfo(address)
+    else:
+        return f"{address} <host:port> 错误的地址"
+
+
+@app.route('/get_mclatency/<address>')
+def get_mclatency(address: str):
+    if is_valid_address(address):
+        return mclatency(address)
+    else:
+        return f"{address} <host:port> 错误的地址"
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/info', methods=['GET'])
+def pcinfo():
+    try:
+        key = request.args.get("key")
+        if key == SECRET_KEY:
+            info_type = request.args.get('type')
+            report_time = request.args.get("report_time")
+            match info_type:
+                case "pc":
+                    running_exe = unquote(request.args.get("running_exe"))
+                    data["pc"].append({"running_exe": running_exe, "report_time": report_time})
+                    limit(data["pc"])
+                case "browser":
+                    title = request.args.get("title")
+                    url = request.args.get("url")
+                    data["browser"].append({"title": title, "url": url, "report_time": report_time})
+                    limit(data["browser"])
+                case "phone":
+                    apps = request.args.get("app")
+                    battery_level = request.args.get("battery_level")
+                    wifi_ssid = request.args.get("wifi_ssid")
+                    data["phone"].append({"app": apps, "battery_level": battery_level,
+                                          "wifi_ssid": wifi_ssid, "report_time": report_time})
+                    limit(data["phone"])
+                case _:
+                    return "unknown type"
+            return "ok"
+        else:
+            return "error key"
+    except Exception as e:
+        return str(e[:100])
+
+
+@app.route('/get_info', methods=['GET'])
+def get_pcinfo():
+    return data
+
+
+@app.teardown_request
+def teardown_request(response_or_error):
+    save_data(data)
+    return response_or_error
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=80)
---
+++ report/report.py
@@ -0,0 +1,151 @@
+import signal
+import time
+from urllib.parse import quote
+
+import pygetwindow as gw
+import requests
+from datetime import datetime
+import logging
+import os
+from logging.handlers import TimedRotatingFileHandler
+import argparse
+import ctypes
+
+parser = argparse.ArgumentParser(description='''定时报告程序，使用pythonw在后台运行(第一次用python前台运行查看是否没问题)
+如 python(w) report.py run -u http://127.0.0.1/pcinfo -k '密钥'
+''')
+subparsers = parser.add_subparsers(dest='command', help='可用的命令')
+parser_status = subparsers.add_parser('status', help='查询进程状态')
+parser_kill = subparsers.add_parser('kill', help='杀死进程')
+parser_run = subparsers.add_parser('run', help='运行定时报告程序')
+parser_run.add_argument('-k', '--key', required=True, help='密钥')
+parser_run.add_argument('-u', '--url', required=True, help='api接口, 如: "http://127.0.0.1/info"')
+parser_run.add_argument('-c', '--cycle_time', help='报告周期(单位秒)', default=600)
+
+args = parser.parse_args()
+if not args.command:
+    print("add -h to help")
+FORMAT = "%(asctime)-15s [%(levelname)s] %(message)s"
+logging.basicConfig(format=FORMAT, level=logging.INFO)
+logger = logging.getLogger("main")
+if not os.path.exists("log"):
+    os.makedirs("log")
+
+# 创建文件日志处理程序
+file_handler = TimedRotatingFileHandler("log/report.log", when="midnight", interval=1, backupCount=10, encoding="utf-8")
+file_handler.setFormatter(logging.Formatter(FORMAT))
+logger.addHandler(file_handler)
+
+kernel32 = ctypes.windll.kernel32
+
+
+# 获取进程句柄
+def GetProcessHandle(pid):
+    PROCESS_QUERY_INFORMATION = 0x0400
+    handle = kernel32.OpenProcess(PROCESS_QUERY_INFORMATION, False, pid)
+    return handle
+
+
+# 检查进程是否存在
+def is_process_running(pid):
+    handle = GetProcessHandle(pid)
+    if handle:
+        kernel32.CloseHandle(handle)
+        return True
+    else:
+        return False
+
+
+def read_pid():
+    try:
+        with open(".pid", 'r') as f:
+            pid = int(f.read())
+        return pid
+    except FileNotFoundError:
+        print("没有找到进程ID文件.")
+
+
+def check_process():
+    try:
+        pid = read_pid()
+        print(f"进程ID: {pid}")
+        if is_process_running(pid):
+            print(f"进程正在运行")
+            return True
+        else:
+            print("进程已停止.")
+            return False
+    except (FileNotFoundError, ValueError):
+        print("没有找到进程ID文件或进程不存在.")
+        return False
+
+
+def kill_process():
+    # 杀死进程
+    try:
+        pid = read_pid()
+        if is_process_running(pid):
+            os.kill(pid, signal.SIGTERM)
+            print(f"进程 pid={pid} 已被杀死.")
+        else:
+            print(f"进程 {pid} 不存在.")
+    except Exception as e:
+        print(f"杀死进程时出错: {e}")
+
+
+def get_active_window_title():
+    active_window = gw.getActiveWindow()
+    if active_window:
+        if "Google Chrome" in active_window.title:
+            return "Google Chrome"
+        if active_window.title == "LockingWindow":
+            return "锁屏"
+        return active_window.title
+    else:
+        return "桌面"
+
+
+# 发送数据到Flask API
+def send_data_to_api(running_exe, report_time):
+    try:
+        url = args.url
+        payload = {
+            'key': args.key,
+            'type': 'pc',
+            'running_exe': quote(running_exe),  # 进行URL编码
+            'report_time': report_time
+        }
+        response = requests.get(url, params=payload)
+        msg = response.text
+    except Exception as e:
+        msg = str(e)
+    return msg
+
+
+# 主程序
+def main():
+    print("start...5")
+    time.sleep(5)
+    while True:
+        title = get_active_window_title()
+        times = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        result = send_data_to_api(title, times)
+        logger.info(f"{title}  {result[:20]}")
+        time.sleep(args.cycle_time)
+
+
+if __name__ == "__main__":
+
+    if args.command == 'run':
+        p = read_pid()
+        if is_process_running(p):
+            print(f"进程 pid={p} 已经在运行勿重复执行。如果这不是本程序（查看任务管理器搜索python），删除.pid文件后重试")
+            exit(0)
+        # 保存进程ID到文件
+        with open('.pid', 'w') as f:
+            f.write(str(os.getpid()))
+        main()
+    elif args.command == 'status':
+        check_process()
+    elif args.command == 'kill':
+        kill_process()
